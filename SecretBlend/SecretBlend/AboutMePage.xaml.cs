3e1360c8641e0833dbae91bd77a99085123e59ec
Vasilisa Kashperko <vasilisika@gmail.com>
2023-02-26 02:03:02 +0300

    Beginning of development

---
+++ SecretBlend/SecretBlend/AboutMePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecretBlend
+{
+    public partial class AboutMePage : Page
+    {
+        public AboutMePage()
+        {
+            InitializeComponent();
+        }
+
+        private void TelegramLink_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://t.me/vasilisa_kashperko") { UseShellExecute = true });
+        }
+
+        private void InstagramLink_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.instagram.com/vasilisa.kashperko/") { UseShellExecute = true });
+        }
+
+        private void VKLink_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://vk.com/vasilisa_kashperko") { UseShellExecute = true });
+        }
+    }
+}
---
+++ SecretBlend/SecretBlend/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
+
+namespace SecretBlend
+{
+    public partial class MainWindow : Window
+    {
+        public SolidColorBrush activeButtonBrush = new SolidColorBrush(Color.FromArgb(63, 0, 0, 0));
+        public SolidColorBrush inactiveButtonBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonHide_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMain_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonMain.Background = activeButtonBrush;
+            ButtonAboutMe.Background = inactiveButtonBrush;
+            ButtonAboutProject.Background = inactiveButtonBrush;
+
+            FrameContainer.Source = new Uri("MainPage.xaml", UriKind.Relative);
+        }
+
+        private void ButtonAboutProject_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonAboutProject.Background = activeButtonBrush;
+            ButtonAboutMe.Background = inactiveButtonBrush;
+            ButtonMain.Background = inactiveButtonBrush;
+
+            FrameContainer.Source = new Uri("AboutProjectPage.xaml", UriKind.Relative);
+        }
+
+        private void ButtonAboutMe_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonAboutMe.Background = activeButtonBrush;
+            ButtonMain.Background = inactiveButtonBrush;
+            ButtonAboutProject.Background = inactiveButtonBrush;
+
+            FrameContainer.Source = new Uri("AboutMePage.xaml", UriKind.Relative);
+        }
+    }
+}
